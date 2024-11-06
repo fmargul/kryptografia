4a6f93798f0fb25b4f7b5547ca6b1fb7d5570621
Joffrey391 <michal2314534@gmail.com>
2024-11-06 03:11:55 +0100

    Conversion of text into numbers and back, adding tests, constants

---
+++ constants.py
@@ -0,0 +1,1 @@
+BLOCK_SIZE = 20  # Maksymalna długość bloku (dla przykładu; zależy od długości klucza RSA)
---
+++ test.py
@@ -0,0 +1,34 @@
+from rsa import *
+from scripts import *
+
+# RSA
+
+p, q = rsa_get_private_key_primes(32)
+print(p, q)
+n = p*q
+print(n)
+euler_n = (p-1)*(q-1)
+print(euler_n)
+e = 65537
+d = rsa_get_private_exponent(e, euler_n)
+print(e, d)
+
+test_public = PublicKey(e,n)
+test_private = PrivateKey(d,p,q)
+
+message = 420
+encrypted = pow(message,test_public.e, test_public.n)
+print(encrypted)
+decrypted = pow(encrypted,test_private.d,(test_private.p*test_private.q))
+print(decrypted)
+
+
+text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+# Conversion of text into numbers (block division)
+numbers = convert_text_to_numbers(text)
+print("Blocks in figures:", numbers)
+
+# Conversion back from numbers to text
+recovered_text = convert_numbers_to_text(numbers)
+print("Recovered text:", recovered_text)
