e9a12167eb3ab708611ef117f99363ea53e9b0de
Shwastoo <jakubszwast@gmail.com>
2024-11-15 15:08:12 +0100

    pow function replaced + minor fixes

--- rsa.py
+++ rsa.py
@@ -50,18 +50,19 @@ def get_RSA_key_pair():
   return PublicKey(e,n), PrivateKey(d,p,q)
 
 def test_RSA_number():
-  
+  print("\nRSA TEST ON NUMBER\n")
 
   test_public, test_private = get_RSA_key_pair()
 
   message = 420
-  encrypted = pow(message,test_public.e, test_public.n)
+  encrypted = modular_exponentiation(message,test_public.e, test_public.n)
   print(encrypted)
-  decrypted = pow(encrypted,test_private.d,(test_private.p*test_private.q))
+  decrypted = modular_exponentiation(encrypted,test_private.d,(test_private.p*test_private.q))
   print(decrypted)
 
 def test_RSA_text():
-  print("\nRSA TEST ON NUMBER\n")
+  print("\nRSA TEST ON TEXT\n")
+
   test_public, test_private = get_RSA_key_pair()
 
   text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
@@ -77,17 +78,17 @@ def test_RSA_text():
 
   # RSA test on a text message ("Lorem Ipsum" using 128-bit keys)
 
-  print("\nRSA TEST ON TEXT\n")
+  
   blocks_encr_RSA = []
   numbers_to_encrypt = convert_text_to_numbers(text)
   print("Blocks in figures:", numbers_to_encrypt, "\n")
   for block in numbers_to_encrypt:
-      blocks_encr_RSA.append(pow(block, test_public.e, test_public.n))
+      blocks_encr_RSA.append(modular_exponentiation(block, test_public.e, test_public.n))
 
   print("Encrypted blocks:", blocks_encr_RSA,"\n")
   blocks_decr_RSA = []
   for block in blocks_encr_RSA:
-      blocks_decr_RSA.append(pow(block,test_private.d,(test_private.p*test_private.q)))
+      blocks_decr_RSA.append(modular_exponentiation(block,test_private.d,(test_private.p*test_private.q)))
   print("Decrypted blocks:", blocks_decr_RSA,"\n")
 
   decrypted_text = convert_numbers_to_text(blocks_decr_RSA)
